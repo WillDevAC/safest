c2b4dd4b5769774b59d39e4b77d0a9c159dc535d
M2Aloma <128236496+M2Aloma@users.noreply.github.com>
2023-06-24 10:38:27 -0500

    feat

--- js/integration/auth.js
+++ js/integration/auth.js
@@ -1,20 +1,59 @@
 const saveTokenWithAuthSuccess = (user, token) => {
     //code.
-}
+};
 
+const isEmptyValuesInput = (email, password) => {
+    return email === "" || password === "" ? true : false;
+}
 
 const Login = (email, password) => {
-    //code.
-}
+  //@Auth logic.
+  $.ajax({
+    url: "",
+    type: "POST",
+    data: {
+      email: email,
+      password: password,
+    },
+  }).done(function (msg) {
+    
+    //success.
+
+  }).fail(function (msg) {
+    $.toast({
+        heading: 'Ooops...',
+        text: 'Usuário inexistente, ou senha incorreta.',
+        showHideTransition: 'plain',
+        icon: 'error',
+        position: 'top-right'
+    })
+  });
+};
 
 const Register = (name, email, password) => {
-    //code.
-}
+  //code.
+};
 
-$('.targetLogin').click(() => {
-    //invoke Login().
+$("#btn-login").click(function () {
+  //invoke Login().
+  const email = $("#email").val();
+  const password = $("#password").val();
+
+  if(isEmptyValuesInput(email, password)) {
+    $.toast({
+        heading: 'Ooops...',
+        text: 'Preencha os campos de email e senha.',
+        showHideTransition: 'plain',
+        icon: 'warning',
+        position: 'top-right'
+    })
+    return;
+  }
+
+  //@Not empty Login() params.
+  alert('não vázios');
 });
 
-$('.targetRegister').click(() => {
-    //invoke Register().
+$(".targetRegister").click(() => {
+  //invoke Register().
 });
