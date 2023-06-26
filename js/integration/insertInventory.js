f1f8177ab11e85c1bf91717d8e19631d8287aa90
kauanyrodrigues19 <kauanyrodrigues.discente@gmail.com>
2023-06-26 15:27:50 -0500

    Inicialização da integração de fotos e inventario

---
+++ js/integration/insertInventory.js
@@ -0,0 +1,58 @@
+const insertInventory = (nomeInve, cidade, estado, rua, bairro, numero, cep, complemento, data) => {
+    // @Auth logic.
+  
+    $.ajax({
+      url: "/refatoracao_safest/controladores/rota.php",
+      type: "POST",
+      data: {
+        controlador: "inventario",
+        acao: "inserir",
+        nomeInve: nomeInve,
+        cidade: cidade,
+        estado: estado,
+        rua: rua,
+        bairro: bairro,
+        numero: numero,
+        cep: cep,
+        complemento: complemento,
+        data: data,
+      },
+    })
+      .done(function (msg) {
+        // Callback function for successful AJAX request.
+        // You can handle the response here.
+      })
+      .fail(function (msg) {
+        // Callback function for failed AJAX request.
+        // You can handle the error here.
+      });
+  };
+  
+  const Register = () => {
+    const nomeInve = $("#nomeInve").val();
+    const cidade = $("#cidade").val();
+    const estado = $("#estado").val();
+    const rua = $("#rua").val();
+    const bairro = $("#bairro").val();
+    const numero = $("#numero").val();
+    const cep = $("#cep").val();
+    const complemento = $("#complemento").val();
+    const data = $("#data").val();
+  
+    if (isEmptyValuesInput(nomeInve, cidade, estado, rua, bairro, numero, cep, complemento, data)) {
+      $.toast({
+        heading: 'Ooops...',
+        text: 'Preencha todos os campos obrigatórios.',
+        showHideTransition: 'plain',
+        icon: 'warning',
+        position: 'top-right'
+      });
+      return;
+    }
+  
+    // Call the insertInventory function with the provided data.
+    insertInventory(nomeInve, cidade, estado, rua, bairro, numero, cep, complemento, data);
+  };
+  
+$("#btn-inven").click(Register);
+  
